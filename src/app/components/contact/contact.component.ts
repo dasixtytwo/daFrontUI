9af0e9fbeecdbf5d591bfa76651cb548a7c33188
Davide Agosti <davide.a@davideagosti.info>
2019-11-20 12:29:53 +0000

    Update layout and Add Icons

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+	faHome,
+	faPhone,
+	faEnvelope,
+	faMobile,
+	faRss
+} from '@fortawesome/free-solid-svg-icons';
+import {
+	faFacebook,
+	faTwitter,
+	faGooglePlus,
+	faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+	faHome = faHome;
+	faPhone = faPhone;
+	faEnvelope = faEnvelope;
+	faMobile = faMobile;
+	faFacebook = faFacebook;
+	faTwitter = faTwitter;
+	faGooglePlus = faGooglePlus;
+	faLinkedin = faLinkedin;
+	faRss = faRss;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
